6b99e696a0d38970a3fa4858462ec3d75ce5ad2e
andrew <andrewbai@usf.edu>
2026-02-03 13:24:18 -0500

    renamed items to purchases

--- cc_3.js
+++ cc_3.js
@@ -3,21 +3,21 @@ let customers = [
 {
     name: "Andrew Wang",
     email: "andrewwang123@yahoo.com",
-    items: ["Fortnite 1000 Vbucks", "Fortnite 5000 Vbucks"],
+    purchases: ["Fortnite 1000 Vbucks", "Fortnite 5000 Vbucks"],
 
 },
 {
     name: "Alan Chen",
     email: "alanchen@yahoo.com",
-    items: ["Fortnite Battle Pass "]
+    purchases: ["Fortnite Battle Pass "]
 
 },
 {
     name: "Relvies Smith",
     email: "relvie123@gmail.com",
-    items: ["Fortnite 10000 Vbucks", "Fortnite Skin 1000 Vbucks"]
+    purchases: ["Fortnite 10000 Vbucks", "Fortnite Skin 1000 Vbucks"]
 
 }]
-// Add and remove data
+// Add and removed data // 
 
 
