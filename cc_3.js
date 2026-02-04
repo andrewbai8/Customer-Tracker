f9e83caaee6f9de54bd82c843af279a01bbe8488
andrew <andrewbai@usf.edu>
2026-02-04 16:44:20 -0500

    added and removed cusstomers - defined purchases line on 43. - add customers shift

--- cc_3.js
+++ cc_3.js
@@ -3,19 +3,19 @@ let customers = [
 {
     name: "Andrew Wang",
     email: "andrewwang123@yahoo.com",
-    purchases: ["Fortnite 1000 Vbucks", "Fortnite 5000 Vbucks"],
+    purchases: ["Fortnite 1000 VBucks", "Fortnite 5000 VBucks"],
 
 },
 {
     name: "Alan Chen",
     email: "alanchen@yahoo.com",
-    purchases: ["Fortnite Battle Pass "]
+    purchases: ["Fortnite Battle Pass"]
 
 },
 {
     name: "Ryan Smith",
     email: "RyanSmith123@gmail.com",
-    purchases: ["Fortnite 10000 Vbucks", "Fortnite Skin 1000 Vbucks"]
+    purchases: ["Fortnite 10000 VBucks", "Fortnite 1000 VBucks"]
 
 }]
 // Add and removed data // 
@@ -27,19 +27,18 @@ purchases: ["Rocket League Game", "Fortnite 2500 VBucks"]
 
 })
 // removed customer // 
-customers.shift({
+customers.shift();
 
+
+// Step 4 
+customers[0].email="alanchen_updated123@gmail.com";
+customers[0].purchases.push("Fortnite STW");
 
 
     
-})
-
-
 
 // 
 console.log(customers);
-console.log(customers.name);
 console.log(customers.email);
+let purchases = ["Fortnite 10000 VBucks","Fortnite 2500 VBucks","Rocket League Game", "Fortnite 1000 VBucks","Fortnite Battle Pass", "Fortnite 5000 VBucks"]
 console.log(purchases);
-
-
